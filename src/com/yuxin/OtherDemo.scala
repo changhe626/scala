3705c44ab24811b5862cb27dcd5650b52385560a
changhe626 <changhe626@163.com>
2017-12-02 12:31:29 +0800

    2017年12月2日12:31:25

--- src/com/yuxin/OtherDemo.scala
+++ src/com/yuxin/OtherDemo.scala
@@ -11,6 +11,17 @@ object OtherDemo extends App{
   println(1 to 10)
   println(1 until 4)
 
+  val list=List(1,2,3,5,6,73,43,7,3,5)
+  //并行计算
+  println(list.par.reduce(_+_))
+  println(list.par.fold(100)(_+_))
+
+  //可变参数
+  def hi(id:Int*)={
+    for(i<-id) println(i)
+  }
+
+
 
 
 
